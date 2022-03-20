d50d843d6fd7146827ee566d4bfef89597957bbc
Mike Doty <mdoty@bitpusher.com>
2022-03-20 13:30:12 -0600

    Update SELECT CONNECTION_ID() intercept to not apply to COM_STMT_PREPARE packets.  Hopefully resolves #2564(introduced by #1797)

--- lib/MySQL_Session.cpp
+++ lib/MySQL_Session.cpp
@@ -6198,9 +6198,10 @@ bool MySQL_Session::handler___status_WAITING_CLIENT_DATA___STATE_SLEEP___MYSQL_C
 		current_hostgroup=qpo->destination_hostgroup;
 		return false;
 	}
-
+	
 	// handle case #1797
-	if ((pkt->size==SELECT_CONNECTION_ID_LEN+5 && strncasecmp((char *)SELECT_CONNECTION_ID,(char *)pkt->ptr+5,pkt->size-5)==0)) {
+	// handle case #2564 - do not intercept "SELECT CONNECTION_ID()" in COM_STMT_PREPARE
+	if ((pkt->size==SELECT_CONNECTION_ID_LEN+5 && (unsigned char)(pkt->ptr+4)!=0x16 && strncasecmp((char *)SELECT_CONNECTION_ID,(char *)pkt->ptr+5,pkt->size-5)==0)) {
 		char buf[32];
 		char buf2[32];
 		sprintf(buf,"%u",thread_session_id);
